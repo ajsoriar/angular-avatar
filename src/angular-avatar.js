a91872a2bb0b65b9f0f3358bae2d2d6e1a3164fd
ajsoriar <ajsoriar@gmail.com>
2016-07-11 23:09:42 +0200

    Implement data binding and add example

--- src/angular-avatar.js
+++ src/angular-avatar.js
@@ -37,13 +37,14 @@
                 fontWeight: '@fontWeight',
                 fontScale: '@fontScale',
                 textShadow: '@textShadow',
+                bind: '@bind',
             },
             link: function(scope, element, attrs) {
 
                 var _long = 45,
                     _picture_resolution = 256,
                     _wrapper = true,
-                    _str = scope.initials || "",
+                    _str = "", //scope.initials || "",
                     _bgcolor = "#000",
                     _textcolor = "#fff",
                     _pixelated = false,
@@ -58,86 +59,100 @@
                     _autoColor = false,
                     _font_weight = 100,
                     _font_scale = 100,
-                    _text_shadow = false;
+                    _text_shadow = false,
+                    _bind = false;
 
-                if (scope.bgcolor != undefined) {
-                    _bgcolor = scope.bgcolor;
-                }
+                function checkValues(){
 
-                if (scope.textColor != undefined) {
-                    _textcolor = scope.textColor;
-                }
-
-                if (scope.wrapper != undefined) {
-                    _wrapper = scope.wrapper;
-                }
-
-                if (scope.pictureResolution != undefined) {
-                    _picture_resolution = scope.pictureResolution;
-                }
-
-                if (scope.width != undefined) {
-                    _long = scope.width;
-                }
-
-                if (scope.pixelated != undefined) {
-                    _pixelated = scope.pixelated;
-                    if ( _pixelated === "true" ) { 
-                        _img_styling += "image-rendering:pixelated; image-rendering:-moz-crisp-edges;"; 
+                    if (scope.bind != undefined){
+                        _bind = scope.bind;
                     }
-                }
 
-                if (scope.roundShape != undefined) {
-                    _roundShape = scope.roundShape;
-                    if ( _roundShape ) _wrapper_styling += "border-radius: "+ _long +"px;";
-                } else {
-                    if ( scope.cornerRadius != undefined ){
-                        _corner_radius = scope.cornerRadius;
-                        _wrapper_styling += "border-radius: "+ _corner_radius +"px;";
+                    if (scope.textColor != undefined) {
+                        _textcolor = scope.textColor;
                     }
-                }
 
-                if (scope.class != undefined) {
-                    _extra_classes = scope.class;
-                }
-
-                if (scope.style != undefined) {
-                    _extra_styles = scope.style;
-                }
-
-                if (scope.string != undefined) {
-                    _str = getInitialsFromString( scope.string );
-                }
-
-                if (scope.pictureFormat === 'jpeg') {
-                    _picture_format = "jpeg";
-                }
-
-                if (scope.colorsPalette != undefined) {
-                    _colors_palette = scope.colorsPalette;
-                }                 
-
-                if (scope.autoColor != undefined) {
-
-                    _autoColor = scope.autoColor;
-                    if ( _autoColor === "true" ) {
-                        var i, lon = _str.length, charIndex=0,colorIndex;
-                        for(i=0; i<lon;i++) charIndex = _str.charCodeAt(i);
-                        colorIndex = charIndex % _colors_palette.length;
-                        _bgcolor = _colors_palette[ colorIndex ];
+                    if (scope.wrapper != undefined) {
+                        _wrapper = scope.wrapper;
                     }
-                }
 
-                if (scope.fontWeight != undefined) {
-                    _font_weight = scope.fontWeight;
-                }
+                    if (scope.pictureResolution != undefined) {
+                        _picture_resolution = scope.pictureResolution;
+                    }
 
-                if (scope.fontScale != undefined) {
-                    _font_scale = scope.fontScale;
-                }
+                    if (scope.width != undefined) {
+                        _long = scope.width;
+                    }
 
-                if (scope.textShadow != undefined) {
-                    _text_shadow = scope.textShadow;
+                    if (scope.pixelated != undefined) {
+                        _pixelated = scope.pixelated;
+                        if ( _pixelated === "true" ) { 
+                            _img_styling += "image-rendering:pixelated; image-rendering:-moz-crisp-edges;"; 
+                        }
+                    }
+
+                    if (scope.roundShape != undefined) {
+                        _roundShape = scope.roundShape;
+                        if ( _roundShape ) _wrapper_styling += "border-radius: "+ _long +"px;";
+                    } else {
+                        if ( scope.cornerRadius != undefined ){
+                            _corner_radius = scope.cornerRadius;
+                            _wrapper_styling += "border-radius: "+ _corner_radius +"px;";
+                        }
+                    }
+
+                    if (scope.class != undefined) {
+                        _extra_classes = scope.class;
+                    }
+
+                    if (scope.style != undefined) {
+                        _extra_styles = scope.style;
+                    }
+
+                    if (scope.initials != undefined) {
+                        _str = scope.initials;
+                    }
+
+                    if (scope.string != undefined) {
+                        _str = getInitialsFromString( scope.string );
+                    }
+
+                    if (scope.pictureFormat === 'jpeg') {
+                        _picture_format = "jpeg";
+                    }
+
+                    if (scope.colorsPalette != undefined) {
+                        _colors_palette = scope.colorsPalette;
+                    }                 
+
+                    if (scope.bgcolor != undefined) {
+                        _bgcolor = scope.bgcolor;
+                    } else {
+
+                        if (scope.autoColor != undefined) {
+
+                            _autoColor = scope.autoColor;
+                            if ( _autoColor === "true" ) {
+                                var i, lon = _str.length, charIndex=0,colorIndex;
+                                for(i=0; i<lon;i++) charIndex = _str.charCodeAt(i);
+                                colorIndex = charIndex % _colors_palette.length;
+                                _bgcolor = _colors_palette[ colorIndex ];
+                            }
+                        }                     
+                    }
+
+                    if (scope.fontWeight != undefined) {
+                        _font_weight = scope.fontWeight;
+                    }
+
+                    if (scope.fontScale != undefined) {
+                        _font_scale = scope.fontScale;
+                    }
+
+                    if (scope.textShadow != undefined) {
+                        _text_shadow = scope.textShadow;
+                    }
+
                 }
 
                 function generateAvatar(name, w, h, bgcolor, textcolor, bgImage) {
@@ -189,14 +204,49 @@
                     return output;
                 };
 
-                var imgData = generateAvatar( _str, _picture_resolution, _picture_resolution, _bgcolor, _textcolor, null);
+                var currentElement = element;
 
-                var html = '';
-                if (_wrapper) html += '<div class="avatar-wrapper '+ _extra_classes +'" style="'+ _wrapper_styling +' width:' + _long + 'px; height:' + _long + 'px; '+ _extra_styles +'">';
-                html += '<img src="' + imgData + '" class="avatar-picture" style="'+ _img_styling +'" width="100%" height="" />';
-                if (_wrapper) html += '</div>';
+                function render(){
 
-                element.replaceWith(html);
+                    var imgData = generateAvatar( _str, _picture_resolution, _picture_resolution, _bgcolor, _textcolor, null);
+
+                    var html = '';
+                    if (_wrapper) html += '<div class="avatar-wrapper '+ _extra_classes +'" style="'+ _wrapper_styling +' width:' + _long + 'px; height:' + _long + 'px; '+ _extra_styles +'">';
+                    html += '<img src="' + imgData + '" class="avatar-picture" style="'+ _img_styling +'" width="100%" height="" />';
+                    if (_wrapper) html += '</div>';
+
+                    var replacementElement = angular.element(html);
+                    currentElement.replaceWith(replacementElement);
+                    currentElement = replacementElement;
+                }
+
+                checkValues();
+                if ( _bind === "true" ){
+
+                    scope.$watch( 'string', function ( value ) {
+
+                        checkValues();
+                        render();
+                    });
+
+                    scope.$watch( 'initials', function ( value ) {
+
+                        checkValues();
+                        render();
+                    });
+
+                    /*
+                    scope.$watch( 'bgcolor', function ( value ) {
+
+                        checkValues();
+                        render();
+                    });
+                    */
+
+                } else {
+
+                    render();
+                }
 
             }
         };
